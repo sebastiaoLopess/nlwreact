5e0101e7e8970858b74a9b634657d2d9357c4d44
sebastiao.lopes <lopescastroneto@gmail.com>
2024-02-07 23:43:47 -0300

    nlw rocket

---
+++ src/app.tsx
@@ -0,0 +1,46 @@
+import logo from "./assets/Logo.svg"
+import { NoteCard } from "./components/node-card"
+
+
+export function App() {
+
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+
+      <img src={logo} alt="logomarca" />
+
+      <form className="w-full">
+        <input 
+          type="text" 
+          placeholder="busque suas notas" 
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700"></div>
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+
+          <div className="rounded-md bg-slate-700 p-5 space-y-3">
+
+              <span className="text-sm font-medium text-slate-200">
+                Adicionar nota
+              </span>
+
+              <p className="text-sm leading-6 text-slate-400">
+                Grave uma nota em audio que sera convertida para texto automaticamente
+              </p>
+          </div>
+
+         <NoteCard note={{
+            date: new Date(),
+            content: "Hello"
+         }}/>
+     
+
+        
+      </div>
+
+    </div>
+  )
+}
---
+++ src/components/node-card.tsx
@@ -0,0 +1,32 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+
+  }
+}
+
+export function NoteCard({note} : NoteCardProps){
+    return(
+      <Dialog.Root>
+        <Dialog.Trigger className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+
+              <span className="text-sm font-medium text-slate-300">
+                {note.date.toISOString()}
+              </span>
+
+              <p className="text-sm leading-6 text-slate-400">
+                  {note.content}
+              </p>
+
+              <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className='inset-0 fixed bg-black/60' />
+          <Dialog.Content>OLA</Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    )
+}
